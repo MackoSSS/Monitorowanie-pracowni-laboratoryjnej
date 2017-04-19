12f5b6ed3542459efb3554cf3b9c24ccccabed65
dlugi21 <adrian.kubaszewski@gmail.com>
2017-04-19 20:34:44 +0200

    Serwer+procesy

---
+++ Serwer/Podstawy_Teleinformatyki_Serwer/Podstawy_teleinformatyki_Serwer/Form2.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace Podstawy_teleinformatyki_Serwer
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.NazwaProcesu = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.TypProcesu = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(268, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(713, 436);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(163, 103);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(181, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(12, 121);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(163, 103);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(181, 159);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "label2";
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.NazwaProcesu,
+            this.TypProcesu});
+            this.listView1.Location = new System.Drawing.Point(268, 454);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(432, 131);
+            this.listView1.TabIndex = 6;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.ColumnClick += new System.Windows.Forms.ColumnClickEventHandler(this.listView1_ColumnClick);
+            // 
+            // NazwaProcesu
+            // 
+            this.NazwaProcesu.Text = "Nazwa Procesu";
+            this.NazwaProcesu.Width = 122;
+            // 
+            // TypProcesu
+            // 
+            this.TypProcesu.Text = "Typ procesu";
+            this.TypProcesu.Width = 90;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1083, 607);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader NazwaProcesu;
+        private System.Windows.Forms.ColumnHeader TypProcesu;
+
+    }
+}
---
+++ Serwer/Podstawy_Teleinformatyki_Serwer/Podstawy_teleinformatyki_Serwer/Form2.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+using System.Diagnostics;
+
+//using System.Windows;
+//using System.Management;
+
+namespace Podstawy_teleinformatyki_Serwer
+{
+    public partial class Form2 : Form
+    {
+        public readonly int portNumber;
+        private TcpClient client;
+        private TcpListener server;
+        private NetworkStream mainStream;
+
+        private readonly Thread Listetning;
+        private readonly Thread GetImage;
+
+        ////////////
+        string procesy="";
+        int refr = 0;
+        string[] ProcSys = { "svchost", "winlogon", "RtHDVCpl", "LogonUI", "csrss", "WUDFHost", "taskhostw", "wininit", "NisSrv", "sihost", "dwm", "app_updater", "SearchUI", "armsvc", "lsass", "MsMpEng", "explorer", "dllhost", "mmc", "services", "ShellExperienceHost", "audiodg", "RuntimeBroker", "sqlwriter", "smss", "System" };
+        /// /////////
+     
+
+        public Form2(int port)
+        {
+            portNumber = port;
+            client = new TcpClient();
+            Listetning = new Thread(StartListening);
+            GetImage = new Thread(ReceiveImage);
+            InitializeComponent();
+        }
+
+        private void StartListening()
+        {
+            while (!client.Connected)
+            {
+                server.Start();
+                client = server.AcceptTcpClient();
+            }
+            GetImage.Start();
+        }
+
+        private void StopListening()
+        {
+            server.Stop();
+            client = null;
+
+            if (Listetning.IsAlive) Listetning.Abort();
+            if (GetImage.IsAlive) GetImage.Abort();
+        }
+
+        private void ReceiveImage()
+        {
+            BinaryFormatter binFormatter = new BinaryFormatter();
+
+            while (client.Connected)
+            {
+                mainStream = client.GetStream();
+                pictureBox1.Image = (Image)binFormatter.Deserialize(mainStream);
+                pictureBox2.Image = (Image)binFormatter.Deserialize(mainStream);
+                pictureBox3.Image = (Image)binFormatter.Deserialize(mainStream);
+
+               // label1.Text = Dns.GetHostEntry("192.168.43.179").HostName.ToString();
+                label1.Invoke(new MethodInvoker(delegate { label1.Text = Dns.GetHostEntry(((IPEndPoint)client.Client.RemoteEndPoint).Address).HostName.ToString(); }));
+                
+                ///////////////////////////////////////////////
+                // Pobieranie procesow
+               
+                if (refr % 50 == 0)
+                {
+                    listView1.Invoke(new MethodInvoker(delegate { listView1.Items.Clear(); }));
+                    ArrayList alist = new ArrayList();
+                    
+                    //Process[] processes = Process.GetProcesses("DESKTOP-UCD1E8F");
+                    //Process[] processes = Process.GetProcesses("USER2-KOMPUTER");
+                    //Process[] processes = Process.GetProcesses("192.168.1.15");
+                    //Process[] processes = Process.GetProcesses("192.168.56.2");
+                    Process[] processes = Process.GetProcesses(Dns.GetHostEntry(((IPEndPoint)client.Client.RemoteEndPoint).Address).HostName.ToString());
+                    bool CzySyst = true;
+                    int i = 0;
+                    foreach (Process process in processes)
+                    {
+
+                        alist.Add(process.ProcessName);
+                        
+                        ListViewItem item = new ListViewItem(alist[i].ToString());
+                        item.ForeColor = Color.Green;
+                        ///////////////////////////////////////
+
+                        for (int g = 0; g < ProcSys.Length;g++)
+                        {
+                            if (alist[i].ToString() == ProcSys[g].ToString())
+                            {
+                                item.ForeColor = Color.Green;
+                                item.SubItems.Add("Systemowy");
+                                CzySyst = true;
+                                break;
+                            }
+                            else
+                            {
+                                CzySyst = false;
+                            }
+                        }
+                        if (CzySyst == false)
+                        {
+                            item.ForeColor = Color.Red;
+                            item.SubItems.Add("Inny");
+                        }
+                        
+                        ////////////////////////////////////////
+                        listView1.Invoke(new MethodInvoker(delegate { listView1.Items.Add(item); }));
+                        i++;
+                    }
+
+                    alist.Clear();
+                    procesy = "";
+                    refr = 0;
+                }
+                refr++;
+               
+                /////////////////////////////////////////////////
+            }
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            server = new TcpListener(IPAddress.Any, 1234);
+            Listetning.Start();
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            base.OnFormClosing(e);
+            StopListening();
+        }
+
+        
+
+        ///////////////////////////
+        ///// sortowanie po kliknieciu na kolumne
+        private void listView1_ColumnClick(object sender, ColumnClickEventArgs e)
+        {
+            this.listView1.ListViewItemSorter = new ListViewItemComparer(e.Column);
+        }
+    }
+    
+    ///////////////////////////
+    ///// Klasa potrzebna do sortowania kolumn
+    class ListViewItemComparer : IComparer
+    {
+        private int col;
+        public ListViewItemComparer()
+        {
+            col = 0;
+        }
+        public ListViewItemComparer(int column)
+        {
+            col = column;
+        }
+        public int Compare(object x, object y)
+        {
+            return String.Compare(((ListViewItem)x).SubItems[col].Text, ((ListViewItem)y).SubItems[col].Text);
+        }
+    }
+
+    ///////////////////////////
+}
